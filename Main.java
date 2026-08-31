5a0c185ba5465b20b5181e0a70a6acd5b273ddb5
EmmanuelRuiz87 <emmanuel20082018@gmail.com>
2026-08-31 15:46:58 -0300

    ahora con rectangulosgit add Main.java

--- Main.java
+++ Main.java
@@ -3,21 +3,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Calcula cuadrados 20000"); //imprimir titulo
+        System.out.println("Calcula rectangulos 30000 (y cuadrados)"); //imprimir titulo
 
         Scanner myObj = new Scanner(System.in); //crear un objeto scanner
 
         try {
-            System.out.println("Ingrese la medida del lado del cuadrado (número entero)");
-            int lado = myObj.nextInt(); // leer input
+            System.out.println("Ingrese la primer medida del lado (número entero)");
+            int lado1 = myObj.nextInt(); // leer input
 
-            if (lado <= 0) {
-                System.out.println("El valor debe ser mayor a 0");
+            System.out.println("Ingrese la segunda medida del lado (número entero)");
+            int lado2 = myObj.nextInt(); // leer input
+
+            if (lado1 <= 0 || lado2 <= 0) {
+                System.out.println("El valor de los lados deben ser mayores a 0");
             } else {
-                int perimetro = lado * 4;
-                int area = lado * lado;
+                int perimetro = lado1 * 2 + lado2 * 2;
+                int area = lado1 * lado2;
 
-                System.out.println("Lado ingresado: " + lado);
+                System.out.println("Lados ingresados: " + lado1 + ", " + lado2);
                 System.out.println("Perimetro: " + perimetro);
                 System.out.println("Área: " + area);
             }
