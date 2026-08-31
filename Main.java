7abb59f987197223d0abc55fc1c017f71468eafd
EmmanuelRuiz87 <emmanuel20082018@gmail.com>
2026-08-31 15:00:32 -0300

    programa listo xdddd

--- Main.java
+++ Main.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
+
 public class Main {
-    public static void main(){
-        System.out.println("Calcula cuadrados 20000");
-        
+    public static void main(String[] args){
+        System.out.println("Calcula cuadrados 20000"); //imprimir titulo
+
+        Scanner myObj = new Scanner(System.in); //crear un objeto scanner
+
+        try {
+            System.out.println("Ingrese la medida del lado del cuadrado (número entero)");
+            int lado = myObj.nextInt(); // leer input
+
+            if (lado <= 0) {
+                System.out.println("El valor debe ser mayor a 0");
+            } else {
+                int perimetro = lado * 4;
+                int area = lado * lado;
+
+                System.out.println("Lado ingresado: " + lado);
+                System.out.println("Perimetro: " + perimetro);
+                System.out.println("Área: " + area);
+            }
+        }
+        catch (Exception e){
+            System.out.println("Error. El valor debe ser entero");
+        }
     }
 }
